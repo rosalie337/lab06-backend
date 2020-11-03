c5751437bd8368fa666d97a451b68b545dbd4489
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-02 19:53:07 -0800

    updated data, create tables, drop tables

---
+++ data/authors.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    author_name: 'Audre Lord',
+    published_books: 18,
+  },
+  {
+    author_name: 'Assata Shakur',
+    published_books: 4,
+  },
+  {
+    author_name: 'Alice Walker',
+    published_books: 36,
+  },
+  {
+    author_name: 'Bell Hooks',
+    published_books: 32,
+  },
+  {
+    author_name: 'Angela Davis',
+    published_books: 21,
+  },
+  {
+    author_name: 'Kimberlé Crenshaw',
+    published_books: 7,
+  },
+  {
+    author_name: 'Octavia Estelle Butler ',
+    published_books: 23,
+  },
+  {
+    author_name: 'Roxane Gay',
+    published_books: 9,
+  },
+  {
+    author_name: 'Toni Morrison',
+    published_books: 34,
+  },
+  {
+    author_name: 'Chimamanda Ngozi Adichie',
+    published_books: 22,
+  },
+  {
+    author_name: 'Charlene Carruthers',
+    published_books: 3,
+  },
+  {
+    author_name: 'Mikki Kendall',
+    published_books: 2,
+  },
+];
