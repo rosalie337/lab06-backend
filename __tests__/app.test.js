9ddcf575e070434f45f859c88bebfba3589113e4
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-03 17:40:25 -0800

    test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,26 +33,7 @@ describe('app routes', () => {
 
     test('returns authors', async() => {
 
-      const expectation = [
-        {
-          author_name: 'Audre Lord',
-          published_books: 18,
-          living: false,
-          born: 'New York, NY'
-        },
-        {
-          author_name: 'Assata Shakur',
-          published_books: 4,
-          living: true,
-          born: 'New York, NY'
-        },
-        {
-          author_name: 'Alice Walker',
-          published_books: 36,
-          living: true,
-          born: 'Eatonton, GA'
-        },
-      ];
+      const expectation = [{ 'id':1, 'author_name':'Audre Lord', 'published_books':18, 'living':false, 'born':'New York, NY', 'owner_id':1 }, { 'id':2, 'author_name':'Assata Shakur', 'published_books':4, 'living':true, 'born':'New York, NY', 'owner_id':1 }, { 'id':3, 'author_name':'Alice Walker', 'published_books':36, 'living':true, 'born':'Eatonton, GA', 'owner_id':1 }, { 'id':4, 'author_name':'Bell Hooks', 'published_books':32, 'living':true, 'born':'Hopkinsville, KY', 'owner_id':1 }, { 'id':5, 'author_name':'Angela Davis', 'published_books':21, 'living':true, 'born':'Birmingham, AL', 'owner_id':1 }, { 'id':6, 'author_name':'Kimberlé Crenshaw', 'published_books':7, 'living':true, 'born':'Canton, OH', 'owner_id':1 }, { 'id':7, 'author_name':'Octavia Estelle Butler ', 'published_books':23, 'living':false, 'born':'Pasadena, CA', 'owner_id':1 }, { 'id':8, 'author_name':'Roxane Gay', 'published_books':9, 'living':true, 'born':'Omaha, NE', 'owner_id':1 }, { 'id':9, 'author_name':'Toni Morrison', 'published_books':34, 'living':false, 'born':'Lorain, OH', 'owner_id':1 }, { 'id':10, 'author_name':'Chimamanda Ngozi Adichie', 'published_books':22, 'living':true, 'born':'Enugu, Nigeria', 'owner_id':1 }, { 'id':11, 'author_name':'Charlene Carruthers', 'published_books':3, 'living':true, 'born':'Chicago, IL', 'owner_id':1 }, { 'id':12, 'author_name':'Mikki Kendall', 'published_books':2, 'living':true, 'born':'Chicago, IL', 'owner_id':1 }];
 
       const data = await fakeRequest(app)
         .get('/authors')
